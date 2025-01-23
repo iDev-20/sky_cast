179cebba524e12b2a5ea7517cdf62601ff58aeec
Iniunam Idorenyin <iniunamid@gmail.com>
2025-01-23 19:28:10 +0000

    first commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:sky_cast/screens/home_page.dart';
+import 'package:sky_cast/services/weather.dart';
+import 'package:sky_cast/utilis/navigation.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    var weatherData = await WeatherModel().getLocationWeather();
+
+    Navigation.navigateToScreen(
+        context: context,
+        screen: HomePage(
+          // locationWeather: weatherData,
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: SpinKitChasingDots(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/location.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: avoid_print
+
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double? longitude;
+  double? latitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
